cca6b7cc4749cea03a374ac501dfcf36d8a2ce0b
Steve Haslam <steve.haslam@timgroup.com>
2017-10-30 15:39:51 +0000

    Support ignoreFailures property as expected of verification tasks

--- src/main/java/com/timgroup/gradle/webpack/MochaTestTask.java
+++ src/main/java/com/timgroup/gradle/webpack/MochaTestTask.java
@@ -2,24 +2,25 @@ package com.timgroup.gradle.webpack;
 
 import java.io.File;
 import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import javax.inject.Inject;
 
 import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputDirectory;
 import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.SkipWhenEmpty;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.VerificationTask;
 import org.gradle.process.internal.ExecAction;
 import org.gradle.process.internal.ExecActionFactory;
 
-public class MochaTestTask extends DefaultTask {
+public class MochaTestTask extends DefaultTask implements VerificationTask {
     private File mainFiles;
     private File testFiles;
     private File testOutput;
     private File mochaOptionsFile;
+    private boolean ignoreFailures;
 
     @InputDirectory
     public File getMainFiles() {
@@ -58,6 +59,17 @@ public class MochaTestTask extends DefaultTask {
         this.testOutput = getProject().file(testOutput);
     }
 
+    @Override
+    @Input
+    public boolean getIgnoreFailures() {
+        return ignoreFailures;
+    }
+
+    @Override
+    public void setIgnoreFailures(boolean ignoreFailures) {
+        this.ignoreFailures = ignoreFailures;
+    }
+
     @Inject
     protected ExecActionFactory getExecActionFactory() {
         throw new UnsupportedOperationException();
@@ -66,11 +78,11 @@ public class MochaTestTask extends DefaultTask {
     @TaskAction
     public void runTests() {
         ExecAction execAction = getExecActionFactory().newExecAction();
-        Map<String, String> environment = new LinkedHashMap<>();
         execAction.setExecutable("node_modules/mocha/bin/mocha");
         execAction.environment("JUNIT_REPORT_PATH", testOutput.toString());
         execAction.environment("JUNIT_REPORT_STACK", "1");
         execAction.setArgs(Arrays.asList("--reporter", "mocha-jenkins-reporter", "--opts", mochaOptionsFile, "--recursive", testFiles));
+        execAction.setIgnoreExitValue(ignoreFailures);
         execAction.execute();
     }
 }
--- src/test/groovy/com/timgroup/gradle/webpack/MochaTestTaskTest.groovy
+++ src/test/groovy/com/timgroup/gradle/webpack/MochaTestTaskTest.groovy
@@ -114,6 +114,60 @@ describe("a thing", () => {
         new File(testProjectDir.root, "build/test-results/mochaTest/test-reports.xml").text.contains("true must be equivalent to false")
     }
 
+    def "ignores failing tests if so configured"() {
+        given:
+        buildFile << """
+plugins {
+  id 'com.timgroup.webpack'
+}
+
+mochaTest {
+  ignoreFailures = true
+}
+"""
+
+        testProjectDir.newFolder("src", "main", "javascript")
+        testProjectDir.newFolder("src", "test", "javascript")
+
+        testProjectDir.newFile("package.json") << """
+{
+  "dependencies": {
+    "mocha": "2.3.2",
+    "mocha-jenkins-reporter": "0.1.9",
+    "must": "0.13.1"
+  }
+}
+"""
+        testProjectDir.newFile("mocha.opts") << """
+"""
+        testProjectDir.newFile("src/main/javascript/Thing.js") << """
+"""
+        testProjectDir.newFile("src/test/javascript/ThingTest.js") << """
+var expect = require("must");
+
+describe("a thing", () => {
+    it("works like this", () => {
+        expect(true).to.eql(false);
+    });
+});
+"""
+
+        when:
+        def result = GradleRunner.create()
+            .withProjectDir(testProjectDir.root)
+            .withArguments("check")
+            .withPluginClasspath()
+            .build()
+
+        then:
+        result.task(":mochaTest").outcome == TaskOutcome.SUCCESS
+        new File(testProjectDir.root, "build/test-results/mochaTest/test-reports.xml").text.contains("a thing")
+        new File(testProjectDir.root, "build/test-results/mochaTest/test-reports.xml").text.contains("works like this")
+        new File(testProjectDir.root, "build/test-results/mochaTest/test-reports.xml").text.contains("tests=\"1\"")
+        new File(testProjectDir.root, "build/test-results/mochaTest/test-reports.xml").text.contains("failures=\"1\"")
+        new File(testProjectDir.root, "build/test-results/mochaTest/test-reports.xml").text.contains("true must be equivalent to false")
+    }
+
     def "skips running tests if source directory does not exist"() {
         given:
         buildFile << """
