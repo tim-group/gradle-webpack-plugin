817a1afc2534dfd4e0ccf2f792b8aee7c72a153a
Steve Haslam <steve.haslam@timgroup.com>
2017-11-09 17:21:28 +0000

    Reuse code to set up package.json et al in tests

--- src/test/groovy/com/timgroup/gradle/webpack/WebpackTaskTest.groovy
+++ src/test/groovy/com/timgroup/gradle/webpack/WebpackTaskTest.groovy
@@ -32,33 +32,7 @@ plugins {
   id 'com.timgroup.webpack'
 }
 """
-
-        testProjectDir.newFolder("src", "main", "javascript")
-        testProjectDir.newFile("package.json") << """
-{
-  "dependencies": {
-    "webpack": "3.6.0"
-  }
-}
-"""
-        testProjectDir.newFile("webpack.config.js") << """
-var webpack = require("webpack");
-var path = require('path');
-function relative(suffix) {
-    return path.resolve(__dirname, suffix);
-}
-module.exports = {
-  context: relative("src/main/javascript"),
-  output: {
-    filename: "[name].js",
-    path: relative("build/site")
-  },
-  entry: "./init"
-};
-"""
-        testProjectDir.newFile("src/main/javascript/init.js") << """
-// this is init.js
-"""
+        typicalJavascriptSetup()
 
         when:
         def result = GradleRunner.create()
@@ -74,7 +48,6 @@ module.exports = {
         new File(testProjectDir.root, "build/site/main.js").bytes == ungzippedBytes(new File(testProjectDir.root, "build/site/main.js.gz"))
     }
 
-
     def "manifest digest can be specified"() {
         given:
         buildFile << """
@@ -86,33 +59,7 @@ webpack {
   manifestDigest = "SHA-1"
 }
 """
-
-        testProjectDir.newFolder("src", "main", "javascript")
-        testProjectDir.newFile("package.json") << """
-{
-  "dependencies": {
-    "webpack": "3.6.0"
-  }
-}
-"""
-        testProjectDir.newFile("webpack.config.js") << """
-var webpack = require("webpack");
-var path = require('path');
-function relative(suffix) {
-    return path.resolve(__dirname, suffix);
-}
-module.exports = {
-  context: relative("src/main/javascript"),
-  output: {
-    filename: "[name].js",
-    path: relative("build/site")
-  },
-  entry: "./init"
-};
-"""
-        testProjectDir.newFile("src/main/javascript/init.js") << """
-// this is init.js
-"""
+        typicalJavascriptSetup()
 
         when:
         def result = GradleRunner.create()
@@ -139,33 +86,7 @@ webpack {
   gzipResources = false
 }
 """
-
-        testProjectDir.newFolder("src", "main", "javascript")
-        testProjectDir.newFile("package.json") << """
-{
-  "dependencies": {
-    "webpack": "3.6.0"
-  }
-}
-"""
-        testProjectDir.newFile("webpack.config.js") << """
-var webpack = require("webpack");
-var path = require('path');
-function relative(suffix) {
-    return path.resolve(__dirname, suffix);
-}
-module.exports = {
-  context: relative("src/main/javascript"),
-  output: {
-    filename: "[name].js",
-    path: relative("build/site")
-  },
-  entry: "./init"
-};
-"""
-        testProjectDir.newFile("src/main/javascript/init.js") << """
-// this is init.js
-"""
+        typicalJavascriptSetup()
 
         when:
         def result = GradleRunner.create()
@@ -190,7 +111,67 @@ webpack {
   generateManifest = false
 }
 """
+        typicalJavascriptSetup()
 
+        when:
+        def result = GradleRunner.create()
+            .withProjectDir(testProjectDir.root)
+            .withArguments("assemble")
+            .withPluginClasspath()
+            .build()
+
+        then:
+        result.task(":webpack").outcome == TaskOutcome.SUCCESS
+        filesIn(new File(testProjectDir.root, "build/site")) == ["main.js", "main.js.map", "main.js.gz", "main.js.map.gz"] as Set
+    }
+
+    def "build fails if webpack config file is missing"() {
+        given:
+        buildFile << """
+plugins {
+  id 'com.timgroup.webpack'
+}
+"""
+        typicalJavascriptSetup()
+        new File(testProjectDir.root, "webpack.config.js").delete()
+
+        when:
+        def result = GradleRunner.create()
+            .withProjectDir(testProjectDir.root)
+            .withArguments("assemble")
+            .withPluginClasspath()
+            .buildAndFail()
+
+        then:
+        result.task(":webpack").outcome == TaskOutcome.FAILED
+    }
+
+    def "task uses alternative configuration file when specified"() {
+        given:
+        buildFile << """
+plugins {
+  id 'com.timgroup.webpack'
+}
+
+webpack {
+  configFile "webpack.alt.config.js"
+}
+"""
+        typicalJavascriptSetup()
+        new File(testProjectDir.root, "webpack.config.js").renameTo(new File(testProjectDir.root, "webpack.alt.config.js"))
+
+        when:
+        def result = GradleRunner.create()
+            .withProjectDir(testProjectDir.root)
+            .withArguments("assemble")
+            .withPluginClasspath()
+            .build()
+
+        then:
+        result.task(":webpack").outcome == TaskOutcome.SUCCESS
+    }
+
+    private def typicalJavascriptSetup() {
         testProjectDir.newFolder("src", "main", "javascript")
         testProjectDir.newFile("package.json") << """
 {
@@ -217,94 +198,5 @@ module.exports = {
         testProjectDir.newFile("src/main/javascript/init.js") << """
 // this is init.js
 """
-
-        when:
-        def result = GradleRunner.create()
-            .withProjectDir(testProjectDir.root)
-            .withArguments("assemble")
-            .withPluginClasspath()
-            .build()
-
-        then:
-        result.task(":webpack").outcome == TaskOutcome.SUCCESS
-        filesIn(new File(testProjectDir.root, "build/site")) == ["main.js", "main.js.map", "main.js.gz", "main.js.map.gz"] as Set
-    }
-
-    def "build fails if webpack config file is missing"() {
-        given:
-        buildFile << """
-plugins {
-  id 'com.timgroup.webpack'
-}
-"""
-
-        testProjectDir.newFolder("src", "main", "javascript")
-        testProjectDir.newFile("package.json") << """
-{
-  "dependencies": {
-    "webpack": "3.6.0"
-  }
-}
-"""
-
-        when:
-        def result = GradleRunner.create()
-            .withProjectDir(testProjectDir.root)
-            .withArguments("assemble")
-            .withPluginClasspath()
-            .buildAndFail()
-
-        then:
-        result.task(":webpack").outcome == TaskOutcome.FAILED
-    }
-
-    def "task uses alternative configuration file when specified"() {
-        given:
-        buildFile << """
-plugins {
-  id 'com.timgroup.webpack'
-}
-
-webpack {
-  configFile "webpack.alt.config.js"
-}
-"""
-
-        testProjectDir.newFolder("src", "main", "javascript")
-        testProjectDir.newFile("package.json") << """
-{
-  "dependencies": {
-    "webpack": "3.6.0"
-  }
-}
-"""
-        testProjectDir.newFile("webpack.alt.config.js") << """
-var webpack = require("webpack");
-var path = require('path');
-function relative(suffix) {
-    return path.resolve(__dirname, suffix);
-}
-module.exports = {
-  context: relative("src/main/javascript"),
-  output: {
-    filename: "[name].js",
-    path: relative("build/site")
-  },
-  entry: "./init"
-};
-"""
-        testProjectDir.newFile("src/main/javascript/init.js") << """
-// this is init.js
-"""
-
-        when:
-        def result = GradleRunner.create()
-            .withProjectDir(testProjectDir.root)
-            .withArguments("assemble")
-            .withPluginClasspath()
-            .build()
-
-        then:
-        result.task(":webpack").outcome == TaskOutcome.SUCCESS
     }
 }
